f73819a12e93cd73f6c4d95ccb9293b2f31efc44
Mark Tiedemann <www.marktiedemann@gmail.com>
2017-07-17 20:47:00 +0200

    Refactor pagination logic and release v0.3.0

--- chargebee-to-csv.js
+++ chargebee-to-csv.js
@@ -8,47 +8,25 @@ const csv = require('json2csv')
 
 const limit = 100
 const resultSet = new Set()
-const offsetList = []
 const flags = minimist(process.argv.slice(2))
 
 doFetch(flags)
 
 function doFetch ({ url, token, file, next_offset }) {
-  // See: https://apidocs.chargebee.com/docs/api/#pagination_and_filtering
+  fetch(buildUrl(url, next_offset), addAuth(token))
+  .then(checkFetch)
+  .then(onFetch({ url, token, file, next_offset }))
+  .catch(onError)
+}
 
+function buildUrl (url, next_offset) {
+  // See: https://apidocs.chargebee.com/docs/api/#pagination_and_filtering
   // first call has no offset
   const offset = next_offset
     ? `&offset=${next_offset}`
     : ''
 
-  fetch(`${url}?limit=${limit}${offset}`, addAuth(token))
-  .then(res => res.status !== 200
-    ? Promise.reject(new Error(res.status))
-    : res.json())
-  .then(body => {
-    
-    // add the results to the result set
-    body.list.forEach(result => resultSet.add(result))
-
-    const offset = body.next_offset
-    // the offset is circular: so if the offset is already in the list
-    // we don't need to fetch anymore data
-    const hasMore = offset && !offsetList.includes(offset)
-    if (hasMore) {
-      offsetList.push(offset)
-      doFetch({ url, token, file, next_offset: offset })
-    } else {
-      // no more data needs to be fetched, write the result set to the csv file
-      const data = csv({ data: Array.from(resultSet), flatten: true })
-      fs.writeFile(file, data, err => {
-        if (err) throw err
-      })
-    }
-  })
-  .catch(err => {
-    console.error('An unexpected error occured:', err)
-    process.exit(1)
-  })
+  return `${url}?limit=${limit}${offset}`
 }
 
 function addAuth (token) {
@@ -60,4 +38,36 @@ function addAuth (token) {
       'Authorization': `Basic ${basic}`
     }
   }
+}
+
+function checkFetch (res) {
+  return res.status !== 200
+    ? Promise.reject(new Error(res.status))
+    : res.json()
+}
+
+function onFetch ({ url, token, file, next_offset }) {
+  return body => {
+    // add the results to the result set
+    body.list.forEach(result => resultSet.add(result))
+
+    const offset = body.next_offset
+    if (offset) {
+      doFetch({ url, token, file, next_offset: offset })
+    } else {
+      writeCsv(resultSet, file)
+    }
+  }
+}
+
+function writeCsv (resultSet, file) {
+  const data = csv({ data: Array.from(resultSet), flatten: true })
+  fs.writeFile(file, data, err => {
+    if (err) throw err
+  })
+}
+
+function onError (err) {
+  console.error('An unexpected error occured:', err)
+  process.exit(1)
 }
